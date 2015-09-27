43aca18b22b8d4e15c033866ea27d01f867f1a48
Zizheng Tai <zizheng.tai@gmail.com>
2015-09-27 04:32:16 -0700

    updated tests

--- test/controllers/ItemsSpec.scala
+++ test/controllers/ItemsSpec.scala
@@ -1,17 +1,19 @@
 package controllers
 
-import play.api.test.{ PlaySpecification, FakeRequest, WithApplication }
+import play.api.http.MimeTypes
 import play.api.libs.json.Json
+import play.api.test.{ PlaySpecification, FakeRequest, WithApplication }
 
 class ItemsSpec extends PlaySpecification {
 
   "Items controller" should {
     "list items" in new WithApplication {
-      route(FakeRequest(controllers.routes.Items.list())) match {
-        case Some(response) =>
-          status(response) must equalTo (OK)
-          //contentAsJson(response) must equalTo (Json.arr())
-        case None => failure
+      val contentTypes = Seq(MimeTypes.HTML, MimeTypes.JSON)
+      contentTypes foreach { contentType =>
+        route(FakeRequest(controllers.routes.Items.list()).withHeaders(ACCEPT -> contentType)) match {
+          case Some(response) => status(response) must equalTo (OK)
+          case None => failure
+        }
       }
     }
   }
